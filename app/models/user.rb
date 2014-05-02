8fe068b031454e9e6583ab997da4dfb519de23fc
ikarus-exe <x_dothacker_x@yahoo.com>
2014-05-01 20:29:57 -0700

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+
+	validates :first_name, :email, presence: true
+	validates_format_of :email, :multiline => true, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i 
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChallengeQs::Application.config.secret_key_base = 'bf78f6cbc0e8852cbd89b56387893cfa81662e6d2da82e5944d1b2ff04f39231f31d8359a8bcf0a0ba5bdb843bb9b2a2166535209f69c9eba9b8fed9821f187b'
